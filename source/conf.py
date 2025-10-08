6a4b31ab07a692fb718978551831596a54b57eed
clandolt <info@christophlandolt.com>
2025-10-08 17:47:28 +0200

    Sphinx Project initialized

---
+++ source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CISPA Machine Learning in Cybersecurity'
+copyright = '2025, Christoph R. Landolt, Mario Fritz'
+author = 'Christoph R. Landolt, Mario Fritz'
+release = 'v0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'nbsphinx',
+	'sphinx.ext.mathjax',
+	'sphinx_rtd_theme',
+    'sphinx_gallery.load_style',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+master_doc = 'index'
+
+highlight_language = 'python3'
+
+nbsphinx_execute_arguments = [
+    "--InlineBackend.figure_formats={'svg', 'pdf'}",
+    "--InlineBackend.rc={'figure.dpi': 96}",
+]
